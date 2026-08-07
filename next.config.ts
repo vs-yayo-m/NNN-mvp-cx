0db59fe9f49ce03d4af905014976b16603104e92
vs-yayo-m <yayox.vs@gmail.com>
2026-08-07 14:37:31 +0545

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
-// /next.config.ts
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
     ],
   },
